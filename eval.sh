10fcf63644060e7c16c2da1fe175b9209ce10cf9
kooWZ <60533182+kooWZ@users.noreply.github.com>
2025-10-02 13:26:50 +0800

    add pip list

--- eval.sh
+++ eval.sh
@@ -1,6 +1,7 @@
 cd autoregressive/train
+pip list &> eval.log
 
-torchrun --nproc_per_node=8 eval.py --config configs/evals/eval_flextok_cfg1_32.yaml &> eval.log
+torchrun --nproc_per_node=8 eval.py --config configs/evals/eval_flextok_cfg1_32.yaml &>> eval.log
 torchrun --nproc_per_node=8 eval.py --config configs/evals/eval_flextok_cfg175_32.yaml &>> eval.log
 torchrun --nproc_per_node=8 eval.py --config configs/evals/eval_flextok_cfg2_32.yaml &>> eval.log
 torchrun --nproc_per_node=8 eval.py --config configs/evals/eval_titok_cfg1.yaml &>> eval.log
