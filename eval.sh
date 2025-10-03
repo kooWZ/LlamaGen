e9c7ae0f593268abb343f27aaba497712e3e44b7
kooWZ <lykong22@m.fudan.edu.cn>
2025-10-03 14:13:07 +0800

    remove pip list

--- eval.sh
+++ eval.sh
@@ -1,5 +1,4 @@
 cd autoregressive/train
-pip list &> eval.log
 
 torchrun --nproc_per_node=8 eval.py --config configs/evals/eval_flextok_cfg1_32.yaml &>> eval.log
 torchrun --nproc_per_node=8 eval.py --config configs/evals/eval_flextok_cfg175_32.yaml &>> eval.log
