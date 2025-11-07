d938d1a3198ad81bf29d88ae4c2d50aa65114323
kooWZ <lykong22@m.fudan.edu.cn>
2025-11-07 10:42:24 +0800

    fix train

--- train.sh
+++ train.sh
@@ -1,16 +1,16 @@
-cd autoregressive/train
-torchrun --nproc_per_node=8 extract_codes_ours.py --data-path "$IMGNET_PATH"
+# cd autoregressive/train
+# torchrun --nproc_per_node=8 extract_codes_ours.py --data-path "$IMGNET_PATH"
 
-cd ../../
+# cd ../../
 
-cd tools
-python convert_npy_to_hdf5.py \
-    --code_dir dataset/ImageNet-1k/ours_0250000_codes/imagenet256_codes \
-    --label_dir dataset/ImageNet-1k/ours_0250000_codes/imagenet256_labels \
-    --output_prefix dataset/ImageNet-1k/ours_0250000_codes/h5_dataset/dataset_rank \
-    --code-len 128
+# cd tools
+# python convert_npy_to_hdf5.py \
+#     --code_dir dataset/ImageNet-1k/ours_0250000_codes/imagenet256_codes \
+#     --label_dir dataset/ImageNet-1k/ours_0250000_codes/imagenet256_labels \
+#     --output_prefix dataset/ImageNet-1k/ours_0250000_codes/h5_dataset/dataset_rank \
+#     --code-len 128
 
-cd ..
+# cd ..
 
 
 cd autoregressive/train
