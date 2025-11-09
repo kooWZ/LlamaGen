898fedc1a9a1874e73832105f707f69617b45b64
kooWZ <lykong22@m.fudan.edu.cn>
2025-11-09 15:19:36 +0800

    add l training

--- train.sh
+++ train.sh
@@ -14,4 +14,4 @@
 
 
 cd autoregressive/train
-torchrun --nproc_per_node=8 train_c2i.py --config configs/ours_0250000_b.yaml
+torchrun --nproc_per_node=8 train_c2i.py --config configs/ours_0250000_l.yaml
